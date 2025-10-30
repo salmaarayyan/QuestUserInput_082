ca42f3a73d0de005e3b63460343f6e99333c9909
salmaarayyan <salmaarayyan@gmail.com>
2025-10-30 12:14:05 +0700

    Menambahkan horizontal divider dengan padding dan ketebalan khusus

--- app/src/main/java/com/example/questuserinput_082/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_082/TeksEdit.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -17,6 +19,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -72,6 +76,13 @@ fun FormDataDiri(modifier: Modifier) {
             onValueChange = {
                 textAlamat = it
             }
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
         )
 
 
