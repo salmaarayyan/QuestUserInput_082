b80e1c9eecf365f054a3c03c72eef083a0d41c2f
salmaarayyan <salmaarayyan@gmail.com>
2025-10-30 11:48:03 +0700

    Membuat layout column dengan modifier

--- app/src/main/java/com/example/questuserinput_082/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_082/TeksEdit.kt
@@ -1,11 +1,16 @@
 package com.example.questuserinput_082
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier){
@@ -21,6 +26,11 @@ fun FormDataDiri(modifier: Modifier){
 
     val gender:List<String> = listOf("Laki-laki","Perempuan")
 
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+    }
 
 
-}
+
