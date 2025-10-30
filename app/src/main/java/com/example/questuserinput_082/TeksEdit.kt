46e17833dc77466b053e6dd725a95ae356e1608d
salmaarayyan <salmaarayyan@gmail.com>
2025-10-30 11:42:42 +0700

    Menambahkan variabel gender digunakan untuk menyimpan daftar jenis kelamin

--- app/src/main/java/com/example/questuserinput_082/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_082/TeksEdit.kt
@@ -19,6 +19,8 @@ fun FormDataDiri(modifier: Modifier){
     var alamat by remember { mutableStateOf("") }
     var jenis by remember { mutableStateOf("") }
 
+    val gender:List<String> = listOf("Laki-laki","Perempuan")
+
 
 
 }
