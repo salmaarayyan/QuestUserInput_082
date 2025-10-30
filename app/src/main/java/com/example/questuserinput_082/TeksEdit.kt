75be1e44fdf7d8291ddac61d2b0368dc84f612ce
salmaarayyan <salmaarayyan@gmail.com>
2025-10-30 12:00:54 +0700

    Menambahkan input field untuk alamat lengkap

--- app/src/main/java/com/example/questuserinput_082/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_082/TeksEdit.kt
@@ -63,8 +63,16 @@ fun FormDataDiri(modifier: Modifier) {
                     Text(item)
                 }
             }
-
         }
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = "Alamat Lengkap") },
+            onValueChange = {
+                textAlamat = it
+            }
+        )
 
 
     }
