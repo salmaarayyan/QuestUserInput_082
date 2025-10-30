6e1a90b71e9bb5430a58b97165a78d3d6d512ff4
salmaarayyan <salmaarayyan@gmail.com>
2025-10-30 11:38:37 +0700

    Menambahkan variabel jenis kelamin untuk mengingat nilai masukan dari keyboard

--- app/src/main/java/com/example/questuserinput_082/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_082/TeksEdit.kt
@@ -12,6 +12,7 @@ fun FormDataDiri(modifier: Modifier){
     //variabel-variabel untuk mengingat nilai masukan dari keyboard
     var textNama by remember { mutableStateOf("") }
     var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
 
 
 
